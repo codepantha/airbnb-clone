319bcd19387b1b6a89c0d717841465846f187449
codepanther <pezechigozirim@gmail.com>
2023-04-28 11:34:10 +0100

    Fix minor bug with modal display

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -82,7 +82,7 @@ const Modal: React.FC<ModalProps> = ({
               showModal ? 'translate-y-0' : 'translate-y-full'
             } ${showModal ? 'opacity-100' : 'opacity-0'}`}
           >
-            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="translate h-full overflow-y-auto lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
               {/* HEADER */}
               <header className="flex items-center p-6 lg:mt-8 rounded-t justify-center relative border-b-[1px]">
                 <button

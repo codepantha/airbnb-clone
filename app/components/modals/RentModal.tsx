18980cdece50c4b2d8f34df18a8c1812338da4a7
codepanther <pezechigozirim@gmail.com>
2023-05-01 08:07:40 +0100

    Implement counters for guests, rooms, and allowed guests

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
+import Counter from '../inputs/Counter';
 
 enum Steps {
   Category = 0,
@@ -47,17 +48,24 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
 
   // dynamically import and re-render Map
-  const Map = useMemo(() => dynamic(() => import('../Map'), {
-    ssr: false
-  }), [location]);
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldDirty: true,
       shouldTouch: true,
-      shouldValidate: true,
+      shouldValidate: true
     });
   };
 
@@ -78,7 +86,7 @@ const RentModal = () => {
   }, [step]);
 
   let bodyContent = (
-    <div className="flex flex-col gap-8">
+    <div className="rent-body">
       <Heading
         title="Which of these best describes your place?"
         subtitle="Pick a category"
@@ -103,8 +111,8 @@ const RentModal = () => {
 
   if (step === Steps.Location) {
     bodyContent = (
-      <div className="flex flex-col gap-8">
-        <Heading 
+      <div className="rent-body">
+        <Heading
           title="Where is your place located?"
           subtitle="Help guests find you!"
         />
@@ -114,7 +122,38 @@ const RentModal = () => {
         />
         <Map center={location?.latlng} />
       </div>
-    )
+    );
+  }
+
+  if (step === Steps.Info) {
+    bodyContent = (
+      <div className="rent-body">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenities do you have?"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue('guestCount', value)}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue('roomCount', value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    );
   }
 
   return (

1c72ce3fa6878b44eed3557281d8892dd9d754e8
codepanther <pezechigozirim@gmail.com>
2023-05-16 10:01:56 +0100

    Create getReservations action for server components

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -9,8 +9,7 @@ import { Range } from 'react-date-range';
 
 import { Container } from '@/app/components';
 import { categories } from '@/app/components/navbar/Categories';
-import { SafeListing, SafeUser } from '@/app/types';
-import { Reservation } from '@prisma/client';
+import { SafeListing, SafeReservation, SafeUser } from '@/app/types';
 import { ListingHead, ListingInfo, ListingReservation } from '@/app/components/listings';
 import useLoginModal from '@/app/hooks/useLoginModal';
 
@@ -20,7 +19,7 @@ const initialDateRange = {
   key: 'selection'
 };
 interface ListingClientProps {
-  reservations?: Reservation[];
+  reservations?: SafeReservation[];
   listing: SafeListing & {
     user: SafeUser;
   };
@@ -50,7 +49,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
 
       dates = [...dates, ...range];
     });
-    
+
     return dates;
   }, [reservations]);
 
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,6 +2,7 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
 import { ClientOnly, EmptyState } from "@/app/components";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingId?: string;
@@ -10,6 +11,7 @@ interface IParams {
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
   const currentUser = await getCurrentUser();
+  const reservations = await getReservations(params);
 
   if (!listing) return (
     <ClientOnly>
@@ -22,6 +24,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
       <ListingClient
         listing={listing}
         currentUser={currentUser}
+        reservations={reservations}
       />
     </ClientOnly>
   )
